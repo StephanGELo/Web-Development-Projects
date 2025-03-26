5885ad46e462f2f3737ea03bb64f22e4598aee1c
Stephan Lefort <lefortsge@gmail.com>
2025-03-26 15:28:43 -0700

    Set up postgres database server connection using environmental variables for security

--- Backend/9.1+Authentication+Lv.1/index.js
+++ Backend/9.1+Authentication+Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import {} from 'dotenv/config'
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: `${process.env.DATABASE_USER}`,
+  password:`${process.env.DATABASE_PASSWORD}`,
+  host:`${process.env.DATABASE_HOST}`,
+  database: `${process.env.DATABASE_NAME}`,
+  port: `${process.env.DATABASE_PORT}`,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,17 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  console.log("username is :", username);
+  console.log("password is :", password);
+  });
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log("username is: ", username);
+  console.log("password is: ", password);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
